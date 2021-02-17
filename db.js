d91d6181547052a397432666678e6156f755ff4d
Christine Su <christine.hui.su@gmail.com>
2021-02-16 22:20:43 -0500

    added heroku env variable

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const { Sequelize, DataTypes, UUID, UUIDV4, STRING } = require('sequelize');
 
-const db = new Sequelize('postgres://localhost/acme-country-club');
+const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/acme-country-club');
 
 const Facility = db.define('facility', {
   id: {
